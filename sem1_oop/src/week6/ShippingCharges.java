c2b3ab78c2ede925cc54e34a8404371f144b64c3
artembatutin <artembatutin@gmail.com>
2016-09-30 12:28:58 -0400

    Did excersive 6 and 9 from java chapiter 4 challenges.
    Shipping charge program and sound medium calculation program. Both
    exteremely easy.

---
+++ sem1_oop/src/week6/ShippingCharges.java
@@ -0,0 +1,24 @@
+package week6;
+
+public class ShippingCharges {
+
+    public static void main(String[] args) {
+        int miles = 600;
+        int charges = miles / 500;
+        double rate = 1;
+        double weight = 2;
+
+
+        if(weight < 2)
+            rate = 1.1;
+        else if(weight < 6)
+            rate = 2.2;
+        else if(weight < 10)
+            rate = 3.7;
+        else if(weight >= 10)
+            rate = 4.8;
+
+        double finalPrice = rate * charges;
+        System.out.println("Weight: " + weight + " miles: " + miles + " will cost you " + finalPrice);
+    }
+}
---
+++ sem1_oop/src/week6/SoundSpeed.java
@@ -0,0 +1,33 @@
+package week6;
+
+import java.util.Scanner;
+
+public class SoundSpeed {
+
+    public static void main(String[] args) {
+        //start
+        Scanner input = new Scanner(System.in);
+
+        //Getting and creating the speed.
+        System.out.println("What would be the distance in feets?");
+        double distance = input.nextDouble();
+        Sound sound = new Sound(distance);
+
+
+        SoundMedium med = null;
+
+        while(true) {
+            //Getting the medium and calculating
+            System.out.println("What would be the medium? [air, water, steel]");
+            med = SoundMedium.get(input.nextLine());
+            if(med != SoundMedium.UNDEFINED) {
+                break;
+            }
+            System.out.println("I did not get that, can you repeat.");
+        }
+
+        System.out.println("The medium distance would be: " + sound.getDistance(med));
+
+    }
+
+}
